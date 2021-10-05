601dc96d583fbd5e97cbec1836ff04ff3dcf0491
Felix <fmcmacaspac@protonmail.com>
2021-10-05 08:14:38 +0800

    🎉 starter

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      {/* <p>Challenge by Frontend Mentor. Coded by Your Name Here. </p> */}
+    </div>
+  )
+}
+
+export default Footer
